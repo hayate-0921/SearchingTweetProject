fcaba99173080419b1280fdacc8039f8cb514337
souta0921 <sota.watanabe.4e@stu.hosei.ac.jp>
2025-12-15 23:45:19 +0900

    キーワードの変更

--- config.py
+++ config.py
@@ -10,7 +10,7 @@ ACCESS_SECRET = os.getenv("ACCESS_SECRET")
 BEARER_TOKEN = os.getenv("BEARER_TOKEN")
 
 MAX_USERS_PER_SEARCH = 6                        # 一度に検索するユーザー数
-KEYWORDS = ["歌枠", "歌ってみた", "カラオケ"]     # 検索キーワード
+KEYWORDS = ["cover", "歌ってみた"]               # 検索キーワード
 LOOKBACK_DAYS = 1                               # 遡る日数
 RETWEET_LIMIT = 5                               # 1日あたりのリツイート上限
 MAX_TWEETS_PER_SEARCH = 5                       # 一度の検索で取得する最大ツイート数
