2206c4a66d82da7d195b27072e4149008a8c8535
souta0921 <sota.watanabe.4e@stu.hosei.ac.jp>
2026-01-03 20:57:32 +0900

    MAX_TWEET_PER_SEARCHの修正

--- config.py
+++ config.py
@@ -120,7 +120,7 @@ KEYWORDS_STREAM = ["カラオケ", "歌枠"]           # STREAMアカウント�
 MAX_USERS_PER_SEARCH = 6                        # 一度に検索するユーザー数
 LOOKBACK_DAYS = 1                               # 遡る日数
 RETWEET_LIMIT = 5                               # 1日あたりのリツイート上限
-MAX_TWEETS_PER_SEARCH = 5                       # 一度の検索で取得する最大ツイート数
+MAX_TWEETS_PER_SEARCH = 10                      # 一度の検索で取得する最大ツイート数
 
 # フォルダ設定
 DATA_DIR = "data"
